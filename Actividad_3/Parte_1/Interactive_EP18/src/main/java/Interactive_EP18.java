a28af7a79bdd9972eda9a84a3c85939d8d5b1187
sebasgavi006 <sgaviriav@unal.edu.co>
2023-09-18 15:54:52 -0500

    Ejercicios de la Actividad 3 Parte 1

---
+++ Actividad_3/Parte_1/Interactive_EP18/src/main/java/Interactive_EP18.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author sgaviriav
+ */
+public class Interactive_EP18 {
+
+    public static void main(String[] args) {
+        Formulario1 form = new Formulario1(); // Se crea un objeto "form" de la clase Formulario
+        form.setVisible(true);              // Se hace visible el formulario como método de la clase/función main
+    }
+}
---
+++ Actividad_3/Parte_1/Interactive_EP19/src/main/java/Interactive_EP19.java
@@ -0,0 +1,11 @@
+/**
+ *
+ * @author sgaviriav
+ */
+public class Interactive_EP19 {
+
+    public static void main(String[] args) {
+        Formulario1 form = new Formulario1();
+        form.setVisible(true);
+    }
+}
