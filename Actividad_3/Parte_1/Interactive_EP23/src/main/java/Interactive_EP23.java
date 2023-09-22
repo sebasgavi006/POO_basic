907493e5d00d938754fde7c97cab6a7b9a429460
sebasgavi006 <sgaviriav@unal.edu.co>
2023-09-22 12:21:05 -0500

    Ejercicios resueltos de la Parte1 Actividad3

---
+++ Actividad_3/Parte_1/Interactive_EP23/src/main/java/Interactive_EP23.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author sgaviriav
+ */
+public class Interactive_EP23 {
+
+    public static void main(String[] args) {
+        Formulario1 form = new Formulario1();
+        form.setVisible(true);
+    }
+}
---
+++ Actividad_3/Parte_1/Interactive_ER10/src/main/java/Interactive_ER10.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author sgaviriav
+ */
+public class Interactive_ER10 {
+
+    public static void main(String[] args) {
+        // Creamos un objeto de la clase Formulario1
+        Formulario1 form = new Formulario1();
+        
+        // Mostramos el formulario con el método setVisible de la clase Formulario1
+        form.setVisible(true);
+    }
+}
