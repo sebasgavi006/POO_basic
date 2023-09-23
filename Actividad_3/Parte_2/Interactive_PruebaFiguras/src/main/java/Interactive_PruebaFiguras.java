b81e91517ed7a156e88933e1cdc90016d819aee1
sebasgavi006 <sgaviriav@unal.edu.co>
2023-09-23 00:02:31 -0500

    Finalización Parte 2 Actividad 3

---
+++ Actividad_3/Parte_2/Interactive_PruebaFiguras/src/main/java/Interactive_PruebaFiguras.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+/**
+ *
+ * @author sgaviriav
+ */
+public class Interactive_PruebaFiguras {
+    
+    public static void main(String[] args) {
+        PruebaFiguras form = new PruebaFiguras();   // Creamos un objeto a partir de la clase constructor del formulario
+        form.setVisible(true);  // Hacemos visible el formulario con el método setVisible
+    }
+}
