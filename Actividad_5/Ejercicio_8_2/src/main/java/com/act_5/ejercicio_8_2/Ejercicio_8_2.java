11ba090e353b6dc5d7017011c6a66ca8b93bf506
sebasgavi006 <sgaviriav@unal.edu.co>
2023-11-17 22:58:58 -0500

    Ejercicio 8.2 de la Actividad 5 Finalizado

---
+++ Actividad_5/Ejercicio_8_2/src/main/java/com/act_5/ejercicio_8_2/Ejercicio_8_2.java
@@ -0,0 +1,23 @@
+package com.act_5.ejercicio_8_2;
+
+/**
+* Esta clase define el punto de ingreso al programa de operaciones
+* sobre notas. Por lo tanto, cuenta con un método main de acceso al
+* programa.
+* @version 1.2/2020
+*/
+public class Ejercicio_8_2 {
+
+    /**
+    * Método main que sirve de punto de entrada al programa
+    */
+    public static void main(String[] args) {
+    VentanaPrincipal miVentanaPrincipal; /* Define la ventana
+    principal */
+    miVentanaPrincipal= new VentanaPrincipal(); /* Crea la ventana
+    principal */
+    miVentanaPrincipal.setVisible(true); /* Establece la ventana
+    como visible */
+    }
+    
+}
