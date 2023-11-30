7dc0b167455c42d1c007be3296e23e509c493d6f
sebasgavi006 <sgaviriav@unal.edu.co>
2023-11-29 23:29:11 -0500

    Finalizacion Actividad 6

---
+++ Actividad_6/FileManagement/src/main/java/com/act_6/filemanagement/FileManagement.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.act_6.filemanagement;
+
+/**
+ *
+ * @author sgaviriav
+ */
+public class FileManagement {
+
+    
+    
+    // Hacemos visible la interfaz de usuario
+    public static void main(String[] args) {
+        Interfaz formulario = new Interfaz();       // Se crea un objeto "form" de la clase Formulario
+        formulario.setVisible(true);              // Se hace visible el formulario como método de la clase/función main
+    }
+}
